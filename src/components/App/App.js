7c75ad93d7f2f7772df83a0fcc07d5debf6132a4
elavaux0 <elavaux0@gmail.com>
2024-03-26 16:03:47 +0100

    correctif commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import Header from '../Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header title="Welcome" />
+      App
     </div>
   );
 }
