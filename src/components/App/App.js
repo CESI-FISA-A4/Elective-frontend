4b2e990b7f1f01420287b99b351d46891cfc4784
Laurine MENNERON <laurine.menneron@gmail.com>
2024-03-26 14:14:14 +0100

    Cleared App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,23 @@
 import './App.css';
-import img from '../../assets/product.svg';
-import Card from '../Card/Card';
+import logo from '../../assets/logo.svg';
 
 function App() {
   return (
     <div className="App">
-        <Card 
-        title="Ma salade" 
-        description="Salade, poulet, fromage..."
-        price= "22,5"
-        rate="4,5"
-        img={img}
-        page={'/'}
-        />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
