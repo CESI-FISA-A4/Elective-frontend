302d7da51308ebd611ed202692596d779648bcc7
Laurine MENNERON <laurine.menneron@gmail.com>
2024-03-26 11:19:59 +0100

    Started card
    Tailwind config
    First version of card

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,17 @@
 import './App.css';
-import logo from '../../assets/logo.svg';
+import img from '../../assets/product.svg';
+import Card from '../Card/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Card 
+        title="Ma salade" 
+        description="Salade, poulet, fromage..."
+        price= "22,5"
+        rate="4,5"
+        img={img}
+        />
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import './Card.css';
+import star from '../../assets/star.svg';
 
-const Card = () => {
-    
-};
+function Card ({img, title, description, price, rate}) {
+
+    return (
+        <div className="card bg-greyColor rounded-lg">
+            <img src={img} alt="product" />
+            <div className= "grid grid-cols-2 gap-x-6 gap-y-6">
+                <h2 className="bg-blue-200 text-left"> {title} </h2>
+                <p className='bg-blue-200 text-right'> {price} € </p>
+            </div>
+            <p className="bg-blue-200 text-left"> {description} </p>
+            <div className='grid grid-cols-2'>
+                <div className='grid grid-cols-2 gap-x-0'>
+                    <img src={star} alt="Star"></img>
+                    <p className="bg-blue-200 text-left mr-6"> {rate} </p> 
+                </div>
+                <button className="rounded bg-blackColor text-whiteColor" onClick={console.log("button works")}> Ajouter </button>
+            </div>
+        </div>
+  );
+}
 
 export default Card;
