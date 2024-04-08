c645f06f4711e84d3add7454e2475ac89d3ebbbd
elavaux0 <elavaux0@gmail.com>
2024-04-08 15:32:35 +0100

    a href to link

--- src/utils/components/Footer/Footer.js
+++ src/utils/components/Footer/Footer.js
@@ -1,4 +1,6 @@
-export default function () {
+import { Link } from 'react-router-dom';
+
+export default function Footer() {
     return(
     <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
         <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
@@ -6,16 +8,16 @@ export default function () {
         </span>
         <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-                <a href="login" class="hover:underline me-4 md:me-6">About</a>
+                <Link to={'/'} class="hover:underline me-4 md:me-6">About</Link>
             </li>
             <li>
-                <a href="signup" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                <Link to={'/'} class="hover:underline me-4 md:me-6">Privacy Policy</Link>
             </li>
             <li>
-                <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                <Link to={'/'} class="hover:underline me-4 md:me-6">Licensing</Link>
             </li>
             <li>
-                <a href="#" class="hover:underline">Contact</a>
+                <Link to={'/'} class="hover:underline">Contact</Link>
             </li>
         </ul>
         </div>
