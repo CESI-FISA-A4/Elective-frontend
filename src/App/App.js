081d63788ed54ee277c3f99f12d52659bfd79b45
Laurine MENNERON <laurine.menneron@viacesi.fr>
2024-04-08 23:36:53 +0200

    Finished contact page and mailto

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,7 @@ import LoginPage from '../pages/LoginPage';
 import SignUpPage from '../pages/SignUpPage';
 import Header from '../utils/components/Header/Header';
 import RestaurantPage from '../pages/RestaurantPage';
+import Contact from '../contactModule/components/Contact/Contact';
 
 
 function App() {
@@ -13,8 +14,10 @@ function App() {
     <div className="app">
       <Router>
         <Header></Header>
+        
         <Routes>
           <Route path="/" element={<LoginPage/>}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
           <Route path="/home" element={
             <RequireAuth>
               <HomePage/>
