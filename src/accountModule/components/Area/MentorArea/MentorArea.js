4b83e7143fce4b27ae28df52c43f983a2d60a7fc
Alexandre DELORME <alexandre.delorme@viacesi.fr>
2024-04-09 10:40:28 +0200

    display user mentor data

--- src/accountModule/components/Area/MentorArea/MentorArea.js
+++ src/accountModule/components/Area/MentorArea/MentorArea.js
@@ -4,7 +4,7 @@ import IconButton from '@mui/material/IconButton';
 import { mentorAccountByCode } from '../../../services/account.service';
 import { useState } from 'react';
 
-function MentorArea({mentorCode}) {
+function MentorArea({identity}) {
     const [mentorCodeTarget, setMentorCodeTarget] = useState("");
 
     const handleMentoring = async() => {
@@ -19,13 +19,19 @@ function MentorArea({mentorCode}) {
     return (
         <div className="mentor-area">
             <h2 className='text-secondaryTitle p-2'>Parrainage</h2>
-            <p className='text-standard'>Mon code : {mentorCode}</p>
+            <p className='text-standard text-left'><strong>Mon code :</strong> {identity.mentorCode}</p>
             <div className="flex flex-row">
                 <TextField className="w-full" value={mentorCodeTarget} onChange={(e) => setMentorCodeTarget(e.target.value)} id="mentor" label="Parrainer un ami" variant="outlined" size="small" />
                 <IconButton onClick={handleMentoring}>
                     <HandshakeIcon></HandshakeIcon>
                 </IconButton>
             </div>
+            <br />
+            {identity.mentorFirstname && 
+                <p className='text-standard text-left'>
+                    <strong>Mon parrain :</strong> {identity.mentorLastname.toUpperCase()} {identity.mentorFirstname}
+                </p>
+            }
         </div>
     );
 }
