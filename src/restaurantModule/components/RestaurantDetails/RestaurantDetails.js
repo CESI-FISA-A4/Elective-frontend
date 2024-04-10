943bc7aaa17a3b9abe4cb4f0376287acf50a2b3e
Alexandre DELORME <alexandre.delorme@viacesi.fr>
2024-04-10 10:29:09 +0200

    detail add infos

--- src/restaurantModule/components/RestaurantDetails/RestaurantDetails.js
+++ src/restaurantModule/components/RestaurantDetails/RestaurantDetails.js
@@ -26,13 +26,21 @@ function RestaurantDetail() {
 
     return (
         <div className="restaurant-detail">
-            <div className="flex flex-row bg-bgGreyColor">
+            <div className="flex flex-row bg-bgGreyColor justify-around p-4">
                 {restaurant.loading &&
                     <>
                         <img src={restaurant.data.imgUrl} alt="restaurant details" />
                         <div className="flex flex-col w-full mx-4">
                             <h1 className="text-mainTitle align-center pb-3">{restaurant.data.name}</h1>
                             <p className="text-left">{restaurant.data.description}</p>
+                        </div>
+                        <div className="flex flex-col h-auto w-1/3 justify-around">
+                            <p>Adresse : {restaurant.data.address}</p>
+                            <p>{restaurant.data.acceptTicket ?
+                                "Tickets restaurants acceptés"
+                                :
+                                "Nous ne prenons pas les tickets restaurants"
+                            }</p>
                         </div>
                     </>
                 }
