9e2eb587e38cb831ab1eebbb03149958347b0a89
elavaux0 <elavaux0@gmail.com>
2024-04-10 16:26:40 +0200

    vue client et delivery todo css

--- src/orderModule/components/Order/Order.js
+++ src/orderModule/components/Order/Order.js
@@ -6,16 +6,16 @@ import { Link } from 'react-router-dom';
 function Order({ data }) {
     return (
         <CardActionArea>
-            <div className="order flex flex-row items-center border-solid border-2 rounded">
-                <img src="https://www.shutterstock.com/image-photo/classic-hamburger-stock-photo-isolated-600nw-2282033179.jpg" />
-                <div className="info flex flex-col w-full">
-                    <Link to={`/delivery-state/${data._id}`}>
+            <Link to={`/delivery-state/${data._id}`}>
+                <div className="order flex flex-row items-center border-solid border-2 rounded">
+                    <img src="https://www.shutterstock.com/image-photo/classic-hamburger-stock-photo-isolated-600nw-2282033179.jpg" />
+                    <div className="info flex flex-col w-full">
                         <h3><strong>Commande {data._id}</strong></h3>
-                    </Link>
-                    <p>Total : {data.totalPrice}€ | {data.date}</p>
-                    <h2>{data.state}</h2>
+                        <p>Total : {data.totalPrice}€ | {data.date}</p>
+                        <h2>{data.state}</h2>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </CardActionArea>
     );
 }
