5f8a0a9319d4bde255f519d439e9a232fd0f1ce9
elavaux0 <elavaux0@gmail.com>
2024-04-11 15:04:15 +0200

    service

--- src/NotificationModule/components/Notification.js
+++ src/NotificationModule/components/Notification.js
@@ -1,45 +1,10 @@
-import { useState, useEffect } from "react";
 import NotificationModal from "../../utils/components/Modal/NotificationModal/NotificationModal";
 import {Button} from '@mui/material'
-import { getCommandeStates } from "../service/notification.service";
-import notificationModel from "../../Model/NotificationModel/NotificationModel";
 
-export default function Notification({ cmdId, cmdInprogr}){
-    const [notificationModal, setNotificatonModal] = useState(false);
-    const [cmdState, setCmdState] = useState(null); 
-    const [notificationInfo, setNotificationInfo]= useState({});
-
-    useEffect(() => {
-        if (cmdInprogr){
-            const fetchData = async () => {
-                try {
-                    let response = await getCommandeStates(cmdId);
-                    if(notificationModal){
-                        setNotificatonModal(false)
-                    }
-                    setNotificatonModal(true);
-                    if(cmdState === undefined || response.data.status.state !== cmdState){
-                        setCmdState(notificationModal.data.status.state);
-                        const UserProfil = localStorage.getItem("roleLabel");
-                        const info = notificationModel(UserProfil,  response.data.status.state);
-                        setNotificationInfo(info);
-                    }
-                } catch (error) {
-                console.error('Error fetching data:', error);
-                }
-            };
-        
-            fetchData();
-            const interval = setInterval(() => {
-                fetchData();
-            }, 10000);
-        
-    
-            return () => clearInterval(interval);
-        }
-        }, []);
-        
-
+export default function Notification({ notificationInfo, notificationModal}){
+    function setNotificatonModal(modal){
+        notificationModal = !modal;
+    }
     return(
         <div>
             <NotificationModal title={notificationInfo.title} 
--- src/utils/components/NotificationPanel/NotificationPanel.js
+++ src/utils/components/NotificationPanel/NotificationPanel.js
@@ -1,90 +1,151 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import { Link } from 'react-router-dom';
+    import * as React from 'react';
+    import Box from '@mui/material/Box';
+    import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+    import IconButton from '@mui/material/IconButton';
+    import List from '@mui/material/List';
+    import ListItem from '@mui/material/ListItem';
+    import ListItemButton from '@mui/material/ListItemButton';
+    import ListItemIcon from '@mui/material/ListItemIcon';
+    import ListItemText from '@mui/material/ListItemText';
+    import NotificationsIcon from '@mui/icons-material/Notifications';
+    import { Link } from 'react-router-dom';
+    import { isDeliveryman, isRestaurantOwner } from '../../../authModule/services/auth.service';
+    import notificationModel from '../../../Model/NotificationModel/NotificationModel';
+    import { getCommandePreparedAvailable, getCommandePreparedCreated, getCommandePreparedDelivery } from '../../services/NotificationPanel.service';
+    import { useEffect } from 'react';
+    import Alert from'@mui/material/Alert';
+    import CircularProgress from '@mui/material/CircularProgress';
+    import Notification from '../../../NotificationModule/components/Notification';
 
-export default function NotificationPanel() {
-    const [state, setState] = React.useState(false);
+    export default function NotificationPanel() {
+        const [state, setState] = React.useState(false);
+        const [commandeState, setCommandeState] = React.useState();
+        let notifications = [];
+        let list;
+        let count = 0;
+        useEffect(() => {
+            const fetchData = async () => {
+                try {
+                    if (isDeliveryman()){
+                        console.log('test');
+                        if(commandeState === undefined){
+                            let NewLivraison = await getCommandePreparedDelivery();
+                            setCommandeState(NewLivraison.data);
+                            console.log("NewLivraison = " + NewLivraison.data)
+                        if(NewLivraison.data.length !== 0){
+                            notifications.push(notificationModel('deliveryman', 'NewLivraison'));
+                        }
+                        }else{
+                            let ReadyToDelivers = await getCommandePreparedAvailable();
+                            setCommandeState(ReadyToDelivers.data);
+                            console.log("ReadyToDelivers = " + ReadyToDelivers.data)
+                            if(ReadyToDelivers.data.length !== 0){
+                                notifications.push(notificationModel('deliveryman', 'ReadyToDelivers'));
+                            }
+                        }
+                    }else if(isRestaurantOwner()){
+                        let NewCommand = await getCommandePreparedCreated();
+                        setCommandeState( NewCommand.data);
+                        console.log("NewCommand = " + NewCommand.data)
+                        if( NewCommand.data.length !== 0){
+                            notifications.push(notificationModel('restaurantOwner', 'NewCommand'));
+                        }
+                    }
+                }  catch (error) {
+                    console.log(error);
+                }
+            }
+            
+            fetchData();
+            const interval = setInterval(() => {
+                fetchData();
+            }, 5000);
+        
 
-    const notifications = [
-        {label: "Notif #1", text: "Oui la notiff"},
-        {label: "Notif #2", text: "Oui la notiff"},
-        {label: "Notif #3", text: "Oui la notiff"},
-        {label: "Notif #4", text: "Oui la notiff"},
-        {label: "Notif #5", text: "Oui la notiff"},
-        {label: "Notif #6", text: "Oui la notiff"},
-        {label: "Notif #7", text: "Oui la notiff"},
-        {label: "Notif #8", text: "Oui la notiff"},
-        {label: "Notif #9", text: "Oui la notiff"},
-        {label: "Notif #10", text: "Oui la notiff"},
-    ]
+            return () => clearInterval(interval);
+        }, []);
+            
+        <Alert icon={<CircularProgress />} variant="filled" severity="info">
+            Aucune nouvelle commande en attente !
+        </Alert>
+        
 
-    let count = 117;
 
-    const toggleDrawer = (open) => (event) => {
-        if (
-            event &&
-            event.type === 'keydown' &&
-            (event.key === 'Tab' || event.key === 'Shift')
-        ) {
-            return;
+        const toggleDrawer = (open) => (event) => {
+            if (
+                event &&
+                event.type === 'keydown' &&
+                (event.key === 'Tab' || event.key === 'Shift')
+            ) {
+                return;
+            }
+
+            setState(open);
+        };
+        function handleOnClick(element){
+            <Notification notificationInfo={element} notificationModal={true}/>
         }
+   
 
-        setState(open);
-    };
-const list = () => (
-        <Box
-            role="presentation"
-            onClick={toggleDrawer(false)}
-            onKeyDown={toggleDrawer(false)}
-        >
-            <List>
-                {notifications.map((link, index) => (
-                    <ListItem key={index} disablePadding>
-                        <Link to={link.path}>
+        const test = [
+            {label: "Notif #1", text: "Oui la notiff"},
+            {label: "Notif #2", text: "Oui la notiff"},
+            {label: "Notif #3", text: "Oui la notiff"},
+            {label: "Notif #4", text: "Oui la notiff"},
+            {label: "Notif #5", text: "Oui la notiff"},
+            {label: "Notif #6", text: "Oui la notiff"},
+            {label: "Notif #7", text: "Oui la notiff"},
+            {label: "Notif #8", text: "Oui la notiff"},
+            {label: "Notif #9", text: "Oui la notiff"},
+            {label: "Notif #10", text: "Oui la notiff"},
+        ]
+    
+        count = 117;
+   
+        // count = notifications.length;
+
+        list = () => (
+            <Box
+                role="presentation"
+                onClick={toggleDrawer(false)}
+                onKeyDown={toggleDrawer(false)}
+            >
+                <List>
+                    {test.map((element) => (
+                        <ListItem key={element.label} onClick={() => handleOnClick(element)} disablePadding>
                             <ListItemButton>
-                                <ListItemIcon>
-                                    {link.icon}
-                                </ListItemIcon>
-                                <ListItemText primary={link.text} />
+                                <ListItemText primary={element.text} />
                             </ListItemButton>
-                        </Link>
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
-    );
+                        </ListItem>
+                    ))}
+                </List>
+            </Box>
+        ); 
 
-    return (
-        <div>
-            <React.Fragment key={"right"}>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 1 / 2 }}
-                    onClick={toggleDrawer(true)}
-                >
-                    <NotificationsIcon sx={{fontSize: 45}} />
-                    <p style={{position: "absolute", alignItems: "center", color: "red", fontSize: 15, fontWeight: "bold"}}>{count}</p>
-                </IconButton>
-                <SwipeableDrawer
-                    anchor={"right"}
-                    open={state}
-                    onClose={toggleDrawer(false)}
-                    onOpen={toggleDrawer(true)}
-                >
-                    {list()}
-                </SwipeableDrawer>
-            </React.Fragment>
-        </div>
-    );
-}
+
+        return (
+            <div>
+                <React.Fragment key={"right"}>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 1 / 2 }}
+                        onClick={toggleDrawer(true)}
+                    >
+                        <NotificationsIcon sx={{fontSize: 45}} />
+                        <p style={{position: "absolute", alignItems: "center", color: "red", fontSize: 15, fontWeight: "bold"}}>{count}</p>
+                    </IconButton>
+                    <SwipeableDrawer
+                        anchor={"right"}
+                        open={state}
+                        onClose={toggleDrawer(false)}
+                        onOpen={toggleDrawer(true)}
+                    >
+                        {list()}
+                    </SwipeableDrawer>
+                </React.Fragment>
+            </div>
+        );
+    }
