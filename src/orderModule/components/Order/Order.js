155c75a476b5a0e7da4547ce6eb373353fd22fbf
Asarck <85520089+Asarck@users.noreply.github.com>
2024-04-11 16:58:43 +0200

    ajout cmd pour les order delivery et restaurant

--- src/orderModule/components/Order/Order.js
+++ src/orderModule/components/Order/Order.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function Order({ data }) {
     return (
         <CardActionArea>
-            <Link to={`/delivery-state/${data._id}`}>
+            <Link to={`/orders/${data._id}`}>
                 <div className="order flex flex-row items-center border-solid border-2 rounded">
                     <img src="https://www.shutterstock.com/image-photo/classic-hamburger-stock-photo-isolated-600nw-2282033179.jpg" />
                     <div className="info flex flex-col w-full">
--- src/orderModule/components/OrderDetails/OrderDetails.js
+++ src/orderModule/components/OrderDetails/OrderDetails.js
@@ -1,4 +1,4 @@
-import { abortOrders, getOrdersById, userPayedOrders } from "../../services/order.service";
+import { abortOrders, getOrdersById, restaurantPreparedOrders, userPayedOrders } from "../../services/order.service";
 import React from "react";
 import { useEffect, useState } from "react"
 import { useParams } from 'react-router-dom';
@@ -11,16 +11,19 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import CustomButton from "../../../utils/components/CustomButton";
 import { useNavigate } from 'react-router-dom';
-import { isUser } from "../../../authModule/services/auth.service";
+import { isDeliveryman, isRestaurantOwner, isUser } from "../../../authModule/services/auth.service";
+import { Card, CardContent, TextField, Typography } from "@mui/material";
+import { validateDelivery } from "../../../deliveryManmodule/services/Delivery.service";
 
 
-function OrderDetails(){
-    const {orderId} = useParams();
+function OrderDetails() {
+    const { orderId } = useParams();
 
     const navigate = useNavigate();
     const [orderInfos, setOrderInfos] = useState([]);
-    const [articleList, setArticleList] =useState([]);
-    const [orderStatus, setOrderStatus] = useState(''); 
+    const [articleList, setArticleList] = useState([]);
+    const [orderStatus, setOrderStatus] = useState('');
+    const [codeValidation, setCodeValidation] = useState('');
 
     useEffect(() => {
         const fetchOrderInfos = async () => {
@@ -34,31 +37,48 @@ function OrderDetails(){
     }, []);
 
 
-    async function cancelOrder(){
-        try{
+    async function cancelOrder() {
+        try {
             let response = await abortOrders(orderId);
             alert("La commande a bien été annulée !");
             setOrderStatus("aborted");
             return response.data;
-        }catch(error){
+        } catch (error) {
             alert("Une erreur s'est produite lors de l'annulation de votre commande. Veuillez réessayer.")
         }
     }
 
-    async function payOrder(){
-        try{
+    async function payOrder() {
+        try {
             alert("Paiement en cours...");
-            let response = await userPayedOrders(orderId);       
+            let response = await userPayedOrders(orderId);
             setTimeout(
-                () => {alert("Paiement validé, commande prise en compte !");},
+                () => { alert("Paiement validé, commande prise en compte !"); },
                 2000);
             setOrderStatus("orderChecking");
             return response;
-        }catch (error){
+        } catch (error) {
             alert("Une erreur s'est produite. Veuillez recommencer");
         }
     }
-
+    async function orderReady() {
+        try {
+            let response = await restaurantPreparedOrders(orderId);
+            setOrderStatus("delivering");
+            return response;
+        } catch (error) {
+            alert("Une erreur s'est produite. Veuillez recommencer");
+        }
+    }
+    async function handleSubmit(e) {
+        try {
+            const response = await validateDelivery(orderId, { code: codeValidation });
+            console.log(response);
+            alert('commande validée !')
+        } catch (error) {
+            alert('code invalide')
+        }
+    }
     return (
         <div className="flex flex-col items-center">
             <h1 className="text-secondaryTitle p-4"> Détail de votre commande </h1>
@@ -85,19 +105,45 @@ function OrderDetails(){
                                 <TableCell>{row.quantity}</TableCell>
                                 <TableCell>{row.article.price}</TableCell>
                                 <TableCell>{row.article.price * row.quantity}</TableCell>
-                            </TableRow> 
+                            </TableRow>
                         ))}
                     </TableBody>
                 </Table>
             </TableContainer>
             <div className="flex flex-row m-5 gap-6">
-                {(orderStatus !== "aborted") && (orderStatus !== "delivered") && isUser(localStorage.getItem("userId")) ? <CustomButton onClick={cancelOrder} children={"Annuler la commande"}/> :null}
-                {orderStatus === "orderCreated" ? <CustomButton onClick={payOrder} children={"Payer la commande"}/> : null}
+                {isUser() && (orderStatus !== "aborted") && (orderStatus !== "delivered") ? <CustomButton onClick={cancelOrder} children={"Annuler la commande"} /> : null}
+                {isUser() && orderStatus === "orderCreated" ? <CustomButton onClick={payOrder} children={"Payer la commande"} /> : null}
+                {isRestaurantOwner() && orderStatus === "preparing" && <CustomButton onClick={orderReady} children={"Commande prête !"} />}
                 <p className="text-secondaryTitle">Total : {orderInfos.totalPrice} €</p>
-            </div>                
+            </div>
             <p className="text-secondaryTitle" > Statut de la commande : {orderStatus} </p>
-
-    </div>
+            <div className="flex flex-row m-5 gap-6">
+                {isDeliveryman() && orderStatus === "delivering" &&
+                    <Card className=' mt-5 mr-5 ml-5 sm:w-3/4 '>
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                                Code de validation
+                            </Typography>
+                            <TextField className="w-full" id="Code" label="Code de Validation" variant="outlined" onChange={(e) => { setCodeValidation(e.target.value) }} />
+                            <CustomButton onClick={handleSubmit} children='valider' />
+                        </CardContent>
+                    </Card>
+                }
+                {isDeliveryman() && 
+                    <Card className=' mt-5 mr-5 ml-5 sm:w-3/4 '>
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                                Adresse de livraison :
+                                <br />
+                                {   
+                                    orderInfos.address
+                                }
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                }
+            </div>
+        </div>
     )
 
 }
